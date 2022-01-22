8e21332f92d8a48e80b69fa77b0eb835e94578c7
Pratyush Choudhary <solixious@gmail.com>
2022-01-22 23:33:13 +0530

    Updated to return entire short url when shorten url is called

--- src/main/java/com/urlshortener/service/UrlShortenerService.java
+++ src/main/java/com/urlshortener/service/UrlShortenerService.java
@@ -2,19 +2,31 @@ package com.urlshortener.service;
 
 import com.urlshortener.model.entity.ShortUrlEntity;
 import com.urlshortener.repository.UrlShortenerRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.net.UnknownHostException;
+
 @Service
+@Slf4j
 public class UrlShortenerService {
 
     @Autowired
     private UrlShortenerRepository repository;
 
-    public String getShortUrl(final String url) {
-        ShortUrlEntity entity = ShortUrlEntity.builder().urlOriginal(url).build();
-        repository.save(entity);
-        return entity.getUrlKey();
+    @Autowired
+    private EnvUtil envUtil;
+
+    public String getShortUrl(final String url) throws Exception {
+        try {
+            ShortUrlEntity entity = ShortUrlEntity.builder().urlOriginal(url).build();
+            repository.save(entity);
+            return envUtil.getServerUrlPrefix() + entity.getUrlKey();
+        } catch (UnknownHostException e) {
+            log.error("An error occurred while retrieving hostname.", e);
+            throw e;
+        }
     }
 
     public String getRedirectUrl(final String key) {
